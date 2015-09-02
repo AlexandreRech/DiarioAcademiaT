9dbcfb57e548015ce1f65a1712bfe4c9cfce14c4
alexandre.rech <alexandre.rech@hotmail.com>
2015-09-01 22:45:36 -0300

    Adicionado os arquivos restantes

---
+++ DiarioAcademiaTests/ProvaDataBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiarioAcademia.Dominio.Tests
+{
+    public class ProvaDataBuilder
+    {
+
+        Prova prova = null;
+
+        public ProvaDataBuilder NaData(DateTime data)
+        {
+            prova = new Prova(data);
+
+            return this;
+        }
+
+        public ProvaDataBuilder ComNotaDe(Aluno maria, int nota)
+        {
+            prova.LancarNota(nota, maria);
+
+            return this;
+        }
+
+        public Prova Build()
+        {
+            return prova;
+        }
+    }
+}
---
+++ DiarioAcademiaTests/ProvaTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace DiarioAcademia.Dominio.Tests
+{
+    [TestClass]
+    public class ProvaTest
+    {
+        [TestMethod]
+        public void Deveria_receber_um_lancamento_de_nota()
+        {
+            Prova prova = new Prova(DateTime.Now);
+
+            prova.LancarNota(10, new Aluno("Rech"));
+
+            prova.Notas.Should().HaveCount(1);
+            prova.Notas[0].Valor.Should().Be(10);
+        }
+
+        [TestMethod]
+        public void Deveria_receber_varios_lancamentos_de_notas()
+        {
+            Prova prova = new Prova(DateTime.Now);
+
+            prova.LancarNota(10, new Aluno("Rech"));
+            prova.LancarNota(10, new Aluno("Carla"));
+
+            prova.Notas.Should().HaveCount(2);
+            prova.Notas[0].Valor.Should().Be(10);
+            prova.Notas[1].Valor.Should().Be(10);
+        }
+
+        [TestMethod]
+        public void Nao_deveria_lancar_2_notas_para_um_aluno()
+        {
+            Prova prova = new Prova(DateTime.Now);
+
+            prova.LancarNota(10, new Aluno("Rech"));
+            prova.LancarNota(9, new Aluno("Rech"));
+
+            prova.Notas.Should().HaveCount(1);
+            prova.Notas[0].Valor.Should().Be(10);
+        }
+
+        [TestMethod]
+        public void Nao_deveria_lancar_nota_para_alunos_reprovados_por_falta()
+        {
+            Prova prova = new Prova(DateTime.Now);
+
+            prova.LancarNota(10, new Aluno("Rech") { Faltas = 6});
+            prova.LancarNota(9, new Aluno("Carla"));
+
+            prova.Notas.Should().HaveCount(1);
+            prova.Notas[0].Valor.Should().Be(9);
+        }
+    }
+}
